efee246c7fed4a6a573b020f163d9de268587f07
Uğur Soğukpınar <sogukpinar.ugur@gmail.com>
2015-04-07 14:45:17 +0300

    Linkli liste güncelleme

--- structures/linked_list.c
+++ structures/linked_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linked_list.h"
 
 
 /*
@@ -10,6 +9,22 @@
 	Önemli olan kısım ilk elemanın adresinin bilinmesidir. 
 	Diğer elemanlara zincir şeklinde erişim yapılır.
 */
+
+
+
+typedef struct NODE
+{
+	unsigned int value; // Biz her bir düğüm için tek veri saklayacağız. 
+	struct NODE *next; // Burada bir sonraki elemanın yerini saklayacağız. Bir sonraki elemanda NODE tipinde olmalı.
+}NODE;
+
+
+
+NODE* insert(NODE *, int); 
+void search(NODE *, int); 
+NODE* delete(NODE *, int); 
+void write(NODE *);
+NODE* readAllElements(NODE *);
 
 
 int main()
